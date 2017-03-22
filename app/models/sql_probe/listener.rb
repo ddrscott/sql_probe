b1351a009b790372225d52f010cc61d800518088
Scott Pierce <scott.pierce@centro.net>
2017-03-22 18:17:57 -0500

    Extract module EventWriter

--- app/models/sql_probe/listener.rb
+++ app/models/sql_probe/listener.rb
@@ -20,7 +20,7 @@ module SqlProbe
         end
         begin
           block.call
-          write_to_file_system(events: events, output_base_path: SqlProbe.output_base_dir)
+          EventWriter.write_to_file_system(request: request, events: events, output_base_path: SqlProbe.output_base_dir)
           events
         ensure
           ActiveSupport::Notifications.unsubscribe(subscription_name)
@@ -28,39 +28,6 @@ module SqlProbe
       else
         block.call
       end
-    end
-
-    def write_to_file_system(events:, output_base_path:)
-      # ignore all Rails introspection noise
-      events.reject!{|e| e.payload[:name] == 'SCHEMA'}
-      return if events.empty?
-
-      # reformat event to be more "readable"
-      events.map!{|e| flatten_event_payload(e) }
-
-      file_id = events.first['transaction_id']
-      output_dir = output_full_dir(output_base_path, request)
-      FileUtils.mkdir_p(output_dir)
-      full_path = File.join(output_dir, "#{file_id}.yml")
-
-      # write contents
-      File.open(full_path, 'w') do |f|
-        params = request.params
-        f << {
-          'name' => "#{params[:controller]}##{params[:action]}",
-          'params' => params,
-          'events' => events
-        }.to_yaml
-      end
-    end
-
-    def output_full_dir(output_base_path, request)
-      File.join(output_base_path, request.params[:controller], request.params[:action], request.method)
-    end
-
-    def flatten_event_payload(event)
-      e = event.as_json
-      e.merge!(e.delete('payload'))
     end
   end
 end
