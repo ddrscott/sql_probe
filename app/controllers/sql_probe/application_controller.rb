8ef27266757e6d0317746c61deef8c31dd295832
Scott Pierce <scott.pierce@centro.net>
2017-03-24 07:29:24 -0500

    Minor tweaks
    - default dummy app to home#index
    - remove scheme from asset urls

--- app/controllers/sql_probe/application_controller.rb
+++ app/controllers/sql_probe/application_controller.rb
@@ -1,6 +1,7 @@
 module SqlProbe
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
     layout 'sql_probe/application'
+
+    protect_from_forgery with: :exception
   end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   mount SqlProbe::Engine => "/sql_probe"
   resources :homes
+  root to: 'homes#index'
 end
