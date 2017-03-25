1faf51a6d2b01c6d131b96fc51da26f46dfd0a92
Scott Pierce <scott.pierce@centro.net>
2017-03-25 09:07:48 -0500

    Rename layout file and use same layout for app and spec/dummy

--- app/controllers/sql_probe/application_controller.rb
+++ app/controllers/sql_probe/application_controller.rb
@@ -1,6 +1,6 @@
 module SqlProbe
   class ApplicationController < ActionController::Base
-    layout 'sql_probe/application'
+    layout 'sql_probe/sql_probe'
 
     protect_from_forgery with: :exception
   end
