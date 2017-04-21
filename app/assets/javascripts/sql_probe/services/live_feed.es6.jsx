d96df814ffadd30186fb41b61323a703928b7fb3
Scott Pierce <scott.pierce@centro.net>
2017-04-21 17:15:29 -0500

    Use custom events to remove duplicate ws listners

--- app/assets/javascripts/sql_probe/services/live_feed.es6.jsx
+++ app/assets/javascripts/sql_probe/services/live_feed.es6.jsx
@@ -1,17 +1,26 @@
+/**
+ * Publishes the following custom event to `document`:
+ * 'sql_probe.data': { name, start_time, duration, events, params }
+ */
 const LiveFeed = {
-  onMessage(receiver) {
-
-    const ws = new WebSocket("ws://" + window.location.host + window.SQL_PROBE_CONFIG.live_feed_path);
+  start(liveFeedPath) {
+    const ws = new WebSocket("ws://" + window.location.host + liveFeedPath);
     ws.onmessage = (event) => {
       if (event.data.length && event.data.startsWith('{')) {
         try {
-          var json = JSON.parse(event.data);
-          receiver(json);
+          var json = JSON.parse(event.data),
+              event = new CustomEvent('sql_probe.data', {detail: json});
+          document.dispatchEvent(event);
         } catch(err) {
           console.error(err);
           console.error('ignoring message: ', event.data);
         }
       }
     }
+  },
+  onMessage(receiver) {
+    document.addEventListener('sql_probe.data', (event) => {
+      receiver(event.detail);
+    }, false);
   }
 }
