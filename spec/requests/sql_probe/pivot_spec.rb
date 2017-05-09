91c1376ea3415fbf1a7990ec70d47dca5e8fc242
Scott Pierce <scott.pierce@centro.net>
2017-05-09 08:33:47 -0500

    Fix spec

--- spec/requests/sql_probe/pivot_spec.rb
+++ spec/requests/sql_probe/pivot_spec.rb
@@ -9,20 +9,24 @@ RSpec.describe 'Pivot', type: :request do
       [{ 'name' => 'homes#index',
          'path' => "#{SqlProbe.output_base_dir}/1493717786.675837.yml",
          'duration' => 19.50437900086399,
-         'mtime' => '2017-05-02T04:40:54.000-05:00',
          'queries' => 2,
          'values' => { 'homes' => 1 } },
        { 'name' => 'homes#index',
          'path' => "#{SqlProbe.output_base_dir}/1493717785.644141.yml",
          'duration' => 266.28787699883105,
-         'mtime' => '2017-05-02T04:39:48.000-05:00',
          'queries' => 2,
          'values' => { 'homes' => 1 } }]
     end
+    let(:body) do
+      JSON.parse(response.body).map do |event|
+        event.except('mtime')
+      end
+    end
+
     it 'gets data' do
       get '/sql_probe/pivot'
       expect(response).to have_http_status(200), "body: #{response.body}"
-      expect(JSON.parse(response.body)).to eq(expected)
+      expect(body).to eq(expected)
     end
   end
 end
